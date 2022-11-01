11c0c7702b421b9eaf52bb2f86fbe5504026b280
Ana Carolina <101834138+ananazario@users.noreply.github.com>
2022-11-01 13:57:13 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                verde: {
+                    primary: '#a5dc95',
+                    secondary: '#86bb76',
+                    hover: '#669a58'
+                },
+            },
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif']    
+            }
+        }
+    }
+}
